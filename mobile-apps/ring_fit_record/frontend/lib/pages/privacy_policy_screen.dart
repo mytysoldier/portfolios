a524c103411d3719de171890ce0bd1635fb05759
mytysoldier <yoshi008t@gmail.com>
2024-07-07 20:18:12 +0900

    update

--- mobile-apps/ring_fit_record/frontend/lib/pages/privacy_policy_screen.dart
+++ mobile-apps/ring_fit_record/frontend/lib/pages/privacy_policy_screen.dart
@@ -11,30 +11,96 @@ class PrivacyPolicyScreen extends StatelessWidget {
 
     return Padding(
       padding: const EdgeInsets.all(16),
-      child: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              l10n.privacyPolicyScreenTitle,
-              style: const TextStyle(fontSize: 32),
-            ),
-            const SizedBox(
-              height: 32,
-              width: 30,
-              child: Divider(
-                color: Colors.brown,
+      child: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                l10n.privacyPolicyScreenTitle,
+                style: const TextStyle(fontSize: 32),
               ),
-            ),
-            Text(l10n.privacyPolicyMainSentenceTitle),
-            const SizedBox(height: 5),
-            Text(
-              l10n.privacyPolicyMainSentenceText,
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 32),
-          ],
+              const SizedBox(
+                height: 32,
+                width: 30,
+                child: Divider(
+                  color: Colors.brown,
+                ),
+              ),
+              Text(l10n.privacyPolicyMainSentenceTitle),
+              const SizedBox(height: 5),
+              Text(
+                l10n.privacyPolicyMainSentenceText,
+                textAlign: TextAlign.center,
+              ),
+              _createSubsentence1(l10n),
+              _createSubsentence2(l10n),
+              _createSubsentence3(l10n),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+
+  Widget _createSubsentence1(L10n l10n) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 40),
+          Text(
+            l10n.privacyPolicySubSentence1Title,
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 16),
+          Text(l10n.privacyPolicySubSentence1Text1),
+          Text(l10n.privacyPolicySubSentence1Text2),
+          Text(l10n.privacyPolicySubSentence1Text3),
+          Text(l10n.privacyPolicySubSentence1Text4),
+          Text(l10n.privacyPolicySubSentence1Text5),
+          Text(l10n.privacyPolicySubSentence1Text6),
+          Text(l10n.privacyPolicySubSentence1Text7),
+        ],
+      ),
+    );
+  }
+
+  Widget _createSubsentence2(L10n l10n) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 40),
+          Text(
+            l10n.privacyPolicySubSentence2Title,
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 16),
+          Text(l10n.privacyPolicySubSentence2Text1),
+          Text(l10n.privacyPolicySubSentence2Text2),
+        ],
+      ),
+    );
+  }
+
+  Widget _createSubsentence3(L10n l10n) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 40),
+          Text(
+            l10n.privacyPolicySubSentence3Title,
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 16),
+          Text(l10n.privacyPolicySubSentence3Text1),
+          Text(l10n.privacyPolicySubSentence3Text2),
+        ],
       ),
     );
   }
