de0426c1eb3b97da4c9feaf5810521dee92a1ee2
mytysoldier <yoshi008t@gmail.com>
2024-08-23 07:56:16 +0900

    【habbit tracker】add unit test

--- web-apps/habbit_tracker/src/components/__tests__/WeekCalendar.test.tsx
+++ web-apps/habbit_tracker/src/components/__tests__/WeekCalendar.test.tsx
@@ -1,7 +1,16 @@
-import { fireEvent, render, screen } from "@testing-library/react";
-import Header from "../Header";
-import Ribbon from "../Ribbon";
+import { render, screen } from "@testing-library/react";
 import WeekCalendar from "../WeekCalendar";
+
+// @fullcalendarのインポートでエラーになるため、該当モジュールをモック
+jest.mock("@fullcalendar/react", () => {
+  const MockCalendar = () => <div role="grid">Mock Calendar</div>;
+  MockCalendar.displayName = "MockCalendar";
+  return MockCalendar;
+});
+
+jest.mock("@fullcalendar/daygrid", () => {
+  return {};
+});
 
 describe("WeekCalendarコンポーネントテスト", () => {
   beforeEach(() => {
@@ -11,5 +20,11 @@ describe("WeekCalendarコンポーネントテスト", () => {
   test("WeekCalendarコンポーネントが描画されること", () => {
     const weekCalendarElement = screen.getByRole("grid");
     expect(weekCalendarElement).toBeInTheDocument();
+  });
+
+  test("ボタンが表示されること", () => {
+    const buttonElement = screen.getByRole("button");
+    expect(buttonElement).toBeInTheDocument();
+    expect(buttonElement).toHaveTextContent("タスクを追加");
   });
 });
