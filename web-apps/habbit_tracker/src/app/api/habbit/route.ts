cec8a3ddb9e038835fb315f08040655b54a33bac
mytysoldier <yoshi008t@gmail.com>
2024-09-05 08:08:52 +0900

    【habbit tracker】タスク削除機能

--- web-apps/habbit_tracker/src/app/api/habbit/route.ts
+++ web-apps/habbit_tracker/src/app/api/habbit/route.ts
@@ -1,4 +1,4 @@
-import { createHabbit, updateHabbit } from "@/lib/prisma/habbit";
+import { createHabbit, deleteHabbit, updateHabbit } from "@/lib/prisma/habbit";
 import { HabbitDto } from "@/models/db/habbitDto";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -31,6 +31,18 @@ export async function PUT(request: NextRequest) {
     return NextResponse.json({ message: "Success", data }, { status: 200 });
   } catch (error) {
     console.error(`[Update Habbit]unexpected error: ${error}`);
+    return NextResponse.json({ message: "Error" }, { status: 500 });
+  }
+}
+
+export async function DELETE(request: NextRequest) {
+  try {
+    const jsonBody = await request.json();
+    // delete habbit
+    const data = await deleteHabbit(jsonBody.id);
+    return NextResponse.json({ message: "Success", data }, { status: 200 });
+  } catch (error) {
+    console.error(`[Delete Habbit]unexpected error: ${error}`);
     return NextResponse.json({ message: "Error" }, { status: 500 });
   }
 }
--- web-apps/habbit_tracker/src/app/api/habbit_activity/route.ts
+++ web-apps/habbit_tracker/src/app/api/habbit_activity/route.ts
@@ -1,5 +1,6 @@
 import {
   createHabbitActivity,
+  deleteHabbitActivities,
   deleteHabbitActivity,
 } from "@/lib/prisma/habbit_activity";
 import { HabbitActivityDto } from "@/models/db/habbitActivityDto";
@@ -33,16 +34,27 @@ export async function DELETE(request: NextRequest) {
   try {
     const jsonBody = await request.json();
     const habbit_activity_ids = jsonBody.ids;
-    console.log(`habbit_activity_ids: ${habbit_activity_ids}`);
-    return NextResponse.json({ message: "Success", data: "" }, { status: 200 });
-    // delete habbit activity
-    // const deletedHabbitActivity = await deleteHabbitActivity(
-    //   jsonBody.habbit_id
-    // );
-    // return NextResponse.json(
-    //   { message: "Success", data: deletedHabbitActivity },
-    //   { status: 200 }
-    // );
+    if (!habbit_activity_ids || habbit_activity_ids.length === 0) {
+      return NextResponse.json({ message: "No IDs provided" }, { status: 400 });
+    }
+
+    if (habbit_activity_ids.length === 1) {
+      const deletedHabbitActivity = await deleteHabbitActivity(
+        habbit_activity_ids[0]
+      );
+      return NextResponse.json(
+        { message: "Success", data: deletedHabbitActivity },
+        { status: 200 }
+      );
+    } else {
+      const deletedHabbitActivities = await deleteHabbitActivities(
+        habbit_activity_ids
+      );
+      return NextResponse.json(
+        { message: "Success", data: deletedHabbitActivities },
+        { status: 200 }
+      );
+    }
   } catch (error) {
     console.error(`[Delete HabbitActivity]unexpected error: ${error}`);
     return NextResponse.json({ message: "Error" }, { status: 500 });
