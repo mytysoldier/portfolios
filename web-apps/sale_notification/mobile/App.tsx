00bc5e92c381b3e1bc755f36195de197e670be9b
mytysoldier <yoshi008t@gmail.com>
2024-11-30 22:03:24 +0900

    【sale notification】mobileコンパイルエラー解消

--- web-apps/sale_notification/mobile/App.tsx
+++ web-apps/sale_notification/mobile/App.tsx
@@ -1,13 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { SharedButton } from "shared";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
-      <SharedButton label="test" onClick={() => alert("")} />
     </View>
   );
 }
