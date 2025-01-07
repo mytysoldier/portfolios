c4f11ed2bf362ceb9064465a4662d9fb10443ddf
mytysoldier <yoshi008t@gmail.com>
2025-01-07 23:25:05 +0900

    【sale notification】モバイル版一覧画面レイアウト検討

--- web-apps/sale_notification/frontend/src/app/list/components/yupForm.ts
+++ web-apps/sale_notification/frontend/src/app/list/components/yupForm.ts
@@ -8,6 +8,7 @@ export const formSchema: yup.ObjectSchema<FormModel> = yup.object().shape({
     .max(5, "${label}は${max}文字以内で入力してください。"),
   saleStatus: yup.string().oneOf(SaleStatus.concat([])),
   startDate: yup.string().label("開始日"),
+  endDate: yup.string().label("終了日"),
 });
 
 export type Form = yup.InferType<typeof formSchema>;
